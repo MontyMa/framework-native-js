12b21451722ac3531e832123ba29e6e06950ad88
Monty <montyma1207@gmail.com>
2017-04-12 23:33:37 +0800

    路由配置完成 app.js 修改完成，添加paging项目； 接下来要配置前端自动化工具

--- app.js
+++ app.js
@@ -1,46 +1,62 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+let express = require('express');
+let path = require('path');
+let favicon = require('serve-favicon');
+let logger = require('morgan');
+let cookieParser = require('cookie-parser');
+let bodyParser = require('body-parser');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+// let index = require('./routes/index');
+// let users = require('./routes/users');
 
-var app = express();
+
+let app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'pug');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
+//静态资源路径
+app.use(express.static(path.join(__dirname, './')));
+
+//配置路由
+
+let router_config = require('./config/router.js');
+
+console.log(router_config);
+
+router_config.forEach(elem => {
+    app.get(elem[0], (req, res) => {
+        res.sendFile(path.join(__dirname, `/views${elem[1]}/index.html`));
+    })
+});
+
+
+// app.use('/', index);
+// app.use('/users', users);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+app.use(function (req, res, next) {
+    let err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
---
+++ config/router.js
@@ -0,0 +1,13 @@
+/**
+ * Created by monty on 2017/4/12.
+ */
+
+
+let router_config = [
+    ['/', '/home-page'],
+    ['/paging', '/paging'],
+];
+
+module.exports = router_config;
+
+
