09fe37ab02f3ca956c5d1577f7882a7e00d17427
马腾飞 <montyma1207@gmail.com>
2017-04-19 18:24:14 +0800

    优化分页

--- views/pages/paging/paging.js
+++ views/pages/paging/paging.js
@@ -2,8 +2,6 @@
  * Created by monty on 2017/4/18.
  */
 import $ from 'jquery';
-import doT from 'dot-js';
-
 import Paging from '../../public/scripts/paging-plugin';
 
 import './paging.less';
@@ -11,5 +9,9 @@ import './paging.less';
 let data_box = document.querySelector('.data_box');
 let paging_point = document.querySelector('.paging_point');
 
+let paging_option = {
+    el: 'paging_point',  //挂载点
 
-new Paging("monty").add(2)._replace();
+};
+
+new Paging("monty");
