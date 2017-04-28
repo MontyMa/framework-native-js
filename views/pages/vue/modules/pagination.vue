0af1d8f738abe26813ac54f6519541e4b92e686b
马腾飞 <montyma1207@gmail.com>
2017-04-28 19:52:59 +0800

    续写分页

--- views/pages/vue/modules/pagination.vue
+++ views/pages/vue/modules/pagination.vue
@@ -1,63 +1,127 @@
 <template>
-    <div id="paging">
-        <ul class="paging_ul">
-            <li>上一页</li>
-            <li>1</li>
-            <li>2</li>
-            <li>3</li>
-            <li>下一页</li>
+    <div id="pagination">
+        <ul class="pagesize_box">
+            <li @click="onPrevClick">上一页</li>
+            <template v-for="(pageNo,index) in paging_number_list">
+                <li>{{pageNo}}</li>
+            </template>
+            <li @click="onNextClick">下一页</li>
         </ul>
     </div>
 </template>
-
 <script>
-    import $ from 'jquery';
     export default{
-        name: 'paging',
+        name: 'pagination',
         props: ['option'],
         data(){
             return {
-                //请求得出的数据
-                request_data: {}
+                paging_number_list: [],
             }
         },
+
+        computed: {},
+
         methods: {
-            //获取数据
-            runRequest(){
-                let option = this.option;
-                $.post(option.url, option.param).done(res => this.getRes = res)
+            //获取分页需要的渲染内容
+            getPagingNoList(list){
+//        let [totalPages, currentPage] = [list.totalPages, list.page.pageNo];
+                let [currentPage, totalPages] = [parseInt(6), parseInt(6)];
+
+                let page_number = [];
+                let firsr_concat = [1, '...'];
+                let last_concat = ['...', totalPages];
+
+                if (totalPages <= 5) {
+                    for (let i = 0; i < totalPages; i++) {
+                        page_number.push(i + 1);
+                    }
+                } else {
+                    if (currentPage === 1) {
+                        page_number = [1, 2, '...', totalPages]
+                    } else if (currentPage === 2) {
+                        for (let i = 0; i < currentPage; i++) {
+                            page_number.push(i + 1);
+                        }
+                        page_number = page_number.concat(last_concat);
+                    } else if (currentPage >= 4 && currentPage <= totalPages - 3) {
+                        page_number = [1, '...', currentPage - 1, currentPage, currentPage + 1, '...', totalPages];
+                    } else if (currentPage >= totalPages - 2 && currentPage === totalPages) {
+                        page_number = [1, '...', totalPages - 1, totalPages]
+                    } else {
+                        page_number = [1, 2, 3, 4, '...', totalPages];
+                    }
+                }
+
+                this.paging_number_list = page_number;
+
+
+//        if (parseInt(currentPage) === 1) {
+//          page_number = [currentPage, currentPage + 1];
+//        } else {
+//          page_number = [currentPage - 1, currentPage, currentPage + 1];
+//          page_number = firsr_concat.concat(page_number);
+//        }
+//
+//        if (parseInt(totalPages) >= 5) {
+//          this.paging_number_list = page_number.concat(last_concat);
+//        } else {
+//          this.paging_number_list = page_number
+//        }
+            },
+
+            //上一页
+            onPrevClick(){
+                console.log('1')
+            },
+            //下一页
+            onNextClick(){
+
+            },
+            // 点击页码刷新数据
+            onPageClick (index) {
+//        this.pageMake(index);
+            },
+            //发起请求
+            getRequest(option){
+                console.log(option);
+                lh_http('post', option.url, option.param).done(response => {
+                    let response_data = response.data;
+
+                    //如果请求失败抛出错误
+                    if (!response.status) {
+                        throw response_data
+                    }
+                    console.log(response_data.list, 12111);
+
+                    this.getPagingNoList(response_data.list);
+
+                    //原封不动的导出请求数据
+                    this.$emit('pagination_response', response)
+                })
             }
         },
-        computed: {
-            getRes: {
-                get(){
-                },
-                set(val){
-                    this.$emit('pagination_data', val)
-                }
-            }
+        beforeMount(){
+            this.getRequest(this.option);
         }
     }
 </script>
 
-<style lang="less" scoped>
-    #paging {
-        .paging_ul {
+<style lang="scss" scoped>
+    #pagination {
+        .pagesize_box {
             font-size: 0;
             li {
                 cursor: pointer;
-                font-size: 14px;
                 line-height: 20px;
-                padding: 0 10px;
-                border: 1px solid #d7d7d7;
+                font-size: 12px;
                 color: #666;
+                padding: 0 6px;
+                border-radius: 2px;
+                border: 1px solid #999999;
                 display: inline-block;
                 vertical-align: middle;
-                &:hover {
-                    border-color: #fb5353;
-                }
                 &:not(:first-child) {
-                    margin-left: 5px;
+                    margin-left: 6px;
                 }
             }
         }
